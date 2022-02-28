0d0648c2e366adcc01d4acac691f70509a3bd28d
FranciscoJRE14 <frarodele@alum.us.es>
2022-02-28 21:41:57 +0100

    Migrado a base de datos PostgresSQL

--- MakeAMate/MakeAMate/settings.py
+++ MakeAMate/MakeAMate/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'MakeAMate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mates',
+        'USER': 'mate',
+        'PASSWORD': 'mate',
+        'HOST': 'localhost',
+        'PORT':5432,
     }
 }
 
