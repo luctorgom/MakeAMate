96ba3ef3b5717c3b608598128e7989cc0dda1dda
FranciscoJRE14 <frarodele@alum.us.es>
2022-03-07 17:45:26 +0100

    Login premilinar arreglado

--- MakeAMate/principal/views.py
+++ MakeAMate/principal/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 
 def login_view(request):
-    template='loggeos/Login_X.html'
+    template='loggeos/login.html'
     if request.method == 'POST':
         form = AuthenticationForm(request.POST)
     else:
