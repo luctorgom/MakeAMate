9db64c3350f67ffe3241aa20ea3617fe4d7334a6
FranciscoJRE14 <frarodele@alum.us.es>
2022-03-07 17:35:35 +0100

    Login premilinar

--- MakeAMate/principal/views.py
+++ MakeAMate/principal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 
-# Create your views here.
+def login_view(request):
+    template='loggeos/Login_X.html'
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+    else:
+        form = AuthenticationForm()
+    return render(request,template,{'form':form})
