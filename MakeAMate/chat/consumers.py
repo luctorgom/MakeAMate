2d9bbd8046389ec66a1dfa3e8551cc998c7c257a
javramarr1 <jramosarrondo@gmail.com>
2022-03-13 15:02:41 +0100

    F-OO6: creación de modelo para el chat y guardado de mensajes

--- MakeAMate/chat/consumers.py
+++ MakeAMate/chat/consumers.py
@@ -1,10 +1,14 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from channels.consumer import AsyncConsumer
+from chat.models import Chat,ChatRoom
+from django.contrib.auth.models import User
 
 
-class ChatConsumer(WebsocketConsumer):
+class ChatConsumer(AsyncConsumer):
     def connect(self):
+        #me = self.scope['user']
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
@@ -28,6 +32,8 @@ class ChatConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        self.store_message(message)
+
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
@@ -46,3 +52,12 @@ class ChatConsumer(WebsocketConsumer):
             "type": "chat_message",
             'message': message
         }))
+    
+    def store_message(self,text):
+        Chat.objects.create(
+            content = text,
+            room = ChatRoom.objects.get(name = self.scope['url_route']['kwargs']['room_name'])
+        )
+
+    #El chatroom se guarda una vez que se envía el primer mensaje, lo suyo sería que cuando se creen grupos se guarde al inicio
+    #Faltan añadir a los usuarios implicados
