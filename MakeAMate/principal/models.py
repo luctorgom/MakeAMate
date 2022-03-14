abd251758f7fccf75a277d52d43d1a5444658f12
luctorgom <luctorgom@alum.us.es>
2022-03-15 00:17:39 +0100

    MODELS CORREGIDO
    Se han corregido algunos fallos y ya puede hacerse la migracion y creación de tablas. También se ha añadido a las installed app el modulo principal.

--- MakeAMate/principal/models.py
+++ MakeAMate/principal/models.py
@@ -9,19 +9,19 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 #class Foto(models.Model):
 
 class Gustos(models.Model):
-    opcionGustos=models.CharField()
+    opcionGustos=models.CharField(max_length=40)
 
     def __str__(self):
         return str(self.opcionGustos)
 
 class Aficiones(models.Model):
-    opcionAficiones=models.CharField()
+    opcionAficiones=models.CharField(max_length=50)
 
     def __str__(self):
         return str(self.opcionAficiones)
 
 class Tags(models.Model):
-    etiqueta=models.CharField()
+    etiqueta=models.CharField(max_length=10)
 
     def __str__(self):
         return str(self.etiqueta)
@@ -32,24 +32,26 @@ class Usuario(models.Model):
     #foto=models.ForeignKey('Foto')
     fecha_nacimiento=models.DateField()
     edad=models.PositiveSmallIntegerField(validators=[MinValueValidator(18), MaxValueValidator(30)])
-    lugar=models.CharField()
-    #nacionalidad=models.CharField()
+    lugar=models.CharField(max_length=10)
+    nacionalidad=models.CharField(max_length=10)
     genero= models.CharField(max_length=1,choices=(('F', 'Femenino'),('M','Masculino'),('O','Otro')))
-    pronombres=models.CharField(max_length=3,choices=(('Ella', 'Ella'),('El','El'),('Elle','Elle')))
-    idiomas=models.enums()
-    universidad=models.enums()
-    estudios=models.CharField()
-    tags=models.ManyToManyField()
-    gustos=models.ManyToManyField()
-    aficiones=models.ManyToManyField()
+    pronombres=models.CharField(max_length=4,choices=(('Ella', 'Ella'),('El','El'),('Elle','Elle')))
+    idiomas=models.CharField(max_length=10,choices=(('ES', 'Español'),('EN','Inglés'),('FR','Francés'),
+                                                    ('DE','Alemán'),('PT','Portugués'),('IT','Italiano'),
+                                                    ('SV','Sueco'),('OT','Otro')))
+    universidad=models.CharField(max_length=40)
+    estudios=models.CharField(max_length=40)
+    tags=models.ManyToManyField(to=Tags)
+    gustos=models.ManyToManyField(to=Gustos)
+    aficiones=models.ManyToManyField(to=Aficiones)
 
     def __str__(self):
         return str(self.usuario, self.lugar)
 
 class Mates(models.Model):
     mate=models.BooleanField(default=NullBooleanField)
-    userEntrada=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
-    userSalida=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    userEntrada=models.ForeignKey(settings.AUTH_USER_MODEL, related_name="entrada", on_delete=models.DO_NOTHING)
+    userSalida=models.ForeignKey(settings.AUTH_USER_MODEL, related_name="salida",on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return str(self.mate, self.userEntrada, self.userSalida)
