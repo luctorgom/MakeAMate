ec22f6a80e6e8a52497b7cbb48a7f1f8eecdc29f
Servandofg12 <servandofg12@gmail.com>
2022-03-15 21:11:06 +0100

    Prueba de despliegue

--- MakeAMate/MakeAMate/settings.py
+++ MakeAMate/MakeAMate/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+
+django_heroku.settings(locals())
