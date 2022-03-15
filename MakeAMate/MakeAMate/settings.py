9db776ed3ecefd4b8d60791d1fa84736a57c5b6e
María García <margarcac1@alum.us.es>
2022-03-15 15:41:55 +0100

    CAMBIOS EN ROOM.HTM Y SU STYLE
    Se han corregido dos fallos detectados y se ha movido el archivo style a
    una carpeta estática.
    
    García Cáceres, María <@margarcac1>

--- MakeAMate/MakeAMate/settings.py
+++ MakeAMate/MakeAMate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,7 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
+STATICFILES_FINDERS = (
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+'django.contrib.staticfiles.finders.FileSystemFinder',
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
