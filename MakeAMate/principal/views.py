21ff67ecf90fb5968583d296e2b22f400a6a69b1
jesrolcad <jesrolcad@alum.us.es>
2022-03-16 18:13:27 +0100

    REDIRECCIÓN A LOGIN
    Si un usuario accede a la página principal sin haber iniciado sesión
    será redirigido a la vista de login.
    
    Roldán Cadena, Jesús <@jesrolcad>

--- MakeAMate/principal/views.py
+++ MakeAMate/principal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def login_view(request):
@@ -21,6 +22,6 @@ def logout_view(request):
     logout(request)
     return redirect(homepage)
 
-
+@login_required(login_url="/login")
 def homepage(request):
     return render(request, 'homepage.html')
