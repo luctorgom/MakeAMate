aabc172c97da195b6b8164ed1f953b49e0f3233f
jesrolcad <jesrolcad@alum.us.es>
2022-03-19 19:14:28 +0100

    TESTS DE LOGIN
    Se ha realizado una prueba positiva y otra negativa del login
    
    Roldán Cadena, Jesús <@jesrolcad>

--- MakeAMate/principal/tests.py
+++ MakeAMate/principal/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
+from django.contrib import auth
 
-# Create your tests here.
+
+class LoginTest(TestCase):
+    def setUp(self):
+        user = User(username='usuario')
+        user.set_password('qwery')
+        user.save()
+        super().setUp()
+
+
+    #Test de inicio de sesión con un usuario existente
+    def test_login_positive(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'usuario', 'pass': 'qwery'})
+        user = auth.get_user(c)
+        self.assertTrue(user.is_authenticated)
+
+
+    #Test de inicio de sesión con un usuario inexistente
+    def test_login_negative(self):
+        c = Client()
+        response = c.post('/login/', {'username': 'inexistente', 'pass': 'inexistente'})
+        user = auth.get_user(c)
+        self.assertFalse(user.is_authenticated)
