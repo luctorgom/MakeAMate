5171724a4a8535bead9f8c952326aea97fe1a09e
jorruirui1 <jorruirui1@alum.us.es>
2022-03-26 13:37:22 +0100

    B-010: MEJORA BASE DE DATOS
    Se han implementado la edad como derivado, el campo he encontrado piso, la tabla oferta, campos de fecha premium y derivado, añadida foto y timestamps en mate
    
    Ruiz-Henestrosa Ruiz, Jorge Luis <@jorruirui1>

--- MakeAMate/principal/models.py
+++ MakeAMate/principal/models.py
@@ -2,19 +2,12 @@ from django.conf import settings
 from django.db import models
 from django.forms import NullBooleanField
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Use
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 
 # Create your models here.
-
-#class Foto(models.Model):
-
-class Gustos(models.Model):
-    opcionGustos=models.CharField(max_length=40)
-
-    def __str__(self):
-        return str(self.opcionGustos)
-
 class Aficiones(models.Model):
     opcionAficiones=models.CharField(max_length=50)
 
@@ -22,19 +15,21 @@ class Aficiones(models.Model):
         return str(self.opcionAficiones)
 
 class Tags(models.Model):
-    etiqueta=models.CharField(max_length=10)
+    etiqueta=models.CharField(max_length=40)
 
     def __str__(self):
         return str(self.etiqueta)
 
+class Foto(models.Model):
+    foto=models.ImageField(label="Foto de perfil",upload_to="static/images/users")
+
 class Usuario(models.Model):
     usuario=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     piso=models.BooleanField()
-    #foto=models.ForeignKey('Foto')
+    foto=models.ForeignKey(Foto, on_delete=models.CASCADE)
     fecha_nacimiento=models.DateField()
-    edad=models.PositiveSmallIntegerField(validators=[MinValueValidator(18), MaxValueValidator(30)])
-    lugar=models.CharField(max_length=10)
-    nacionalidad=models.CharField(max_length=10)
+    lugar=models.CharField(max_length=40)
+    nacionalidad=models.CharField(max_length=20)
     genero= models.CharField(max_length=1,choices=(('F', 'Femenino'),('M','Masculino'),('O','Otro')))
     pronombres=models.CharField(max_length=4,choices=(('Ella', 'Ella'),('El','El'),('Elle','Elle')))
     idiomas=models.CharField(max_length=10,choices=(('ES', 'Español'),('EN','Inglés'),('FR','Francés'),
@@ -43,8 +38,22 @@ class Usuario(models.Model):
     universidad=models.CharField(max_length=40)
     estudios=models.CharField(max_length=40)
     tags=models.ManyToManyField(to=Tags)
-    gustos=models.ManyToManyField(to=Gustos)
     aficiones=models.ManyToManyField(to=Aficiones)
+
+    piso_encontrado=models.BooleanField(default=False)
+    fecha_premium=models.DateTimeField()
+
+    @classmethod
+    def get_edad(cls):
+        today = date.today()
+        return today.year - cls.fecha_nacimiento.year - ((today.month, today.day) < (cls.fecha_nacimiento.month, cls.fecha_nacimiento.day))
+
+    @classmethod
+    def es_premium(cls):
+        today = datetime.time
+        fecha_premium_fin = cls.fecha_premium + relativedelta(months=+1)
+
+        return True if fecha_premium_fin > today else False
 
     def __str__(self):
         return str(self.usuario)
@@ -53,9 +62,15 @@ class Mates(models.Model):
     mate=models.BooleanField(default=NullBooleanField)
     userEntrada=models.ForeignKey(settings.AUTH_USER_MODEL, related_name="entrada", on_delete=models.CASCADE)
     userSalida=models.ForeignKey(settings.AUTH_USER_MODEL, related_name="salida",on_delete=models.CASCADE)
+    fecha_mate=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.userEntrada.username + "-" + self.userSalida.username + ": " + str(self.mate))
     
     class Meta:
         unique_together = ('userEntrada', 'userSalida',)
+
+class Ofertas(models.Model):
+    precio=models.IntegerField()
+    descuento=models.FloatField(default=0)
+    duracion_meses=models.IntegerField()
