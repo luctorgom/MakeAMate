f5aa4e508e4cf71a6fa5816ee808ab8ffdbae274
jorruirui1 <jorruirui1@alum.us.es>
2022-03-27 18:59:18 +0200

    B-010: MODELS IMPLEMENTADO CORRECTAMENTE 8
    Se ha implementado que se puedan elegir múltiples idiomas. Se ha detectado un error por el que es necesario borrar la base de datos y rellenarla desde 0
    
    Ruiz-Henestrosa Ruiz, Jorge Luis <@jorruirui1>

--- MakeAMate/principal/tests.py
+++ MakeAMate/principal/tests.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 # Test mates
 class MateTestCase(TestCase):
     def setUp(self):
-        # Modificar si se borra el campo edad...
+
         self.user1 = User(id=0,username="us1")
         self.user1.set_password('123')
         self.user2 = User(id=1,username="us2")
@@ -17,11 +17,11 @@ class MateTestCase(TestCase):
         self.user3 = User(id=2,username="us3")
         self.user3.set_password('123')
 
-        perfil1 = Usuario(usuario=self.user1,piso=True,fecha_nacimiento="2000-1-1",edad=1,lugar="Sevilla",nacionalidad="Española",
+        perfil1 = Usuario(usuario=self.user1,piso=True,fecha_nacimiento="2000-1-1",lugar="Sevilla",nacionalidad="Española",
                             genero='F',pronombres="Ella",idiomas="ES",universidad="US",estudios="Informática")
-        perfil2 = Usuario(usuario=self.user2,piso=True,fecha_nacimiento="2000-1-1",edad=1,lugar="Sevilla",nacionalidad="Española",
+        perfil2 = Usuario(usuario=self.user2,piso=True,fecha_nacimiento="2000-1-1",lugar="Sevilla",nacionalidad="Española",
                             genero='F',pronombres="Ella",idiomas="ES",universidad="US",estudios="Informática")
-        perfil3 = Usuario(usuario=self.user3,piso=True,fecha_nacimiento="2000-1-1",edad=1,lugar="Sevilla",nacionalidad="Española",
+        perfil3 = Usuario(usuario=self.user3,piso=True,fecha_nacimiento="2000-1-1",lugar="Sevilla",nacionalidad="Española",
                             genero='F',pronombres="Ella",idiomas="ES",universidad="US",estudios="Informática")
         
         mate = Mates(userEntrada=self.user3, userSalida=self.user1, mate=True)
@@ -140,13 +140,10 @@ class FiltesTests(TestCase):
         etiquetas= Tags.objects.create(etiqueta="No fumador")
         aficion= Aficiones.objects.create(opcionAficiones="Deportes")
         
-        Pepe= Usuario.objects.create(usuario=userPepe, piso=False, fecha_nacimiento=date(2000,12,31),
-        edad=20,lugar="Sevilla")
-        Maria=Usuario.objects.create(usuario=userMaria, piso=True, fecha_nacimiento=date(2000,12,30),
-        edad=20,lugar="Sevilla")
+        Pepe= Usuario.objects.create(usuario=userPepe, piso=False, fecha_nacimiento=date(2000,12,31),lugar="Sevilla")
+        Maria=Usuario.objects.create(usuario=userMaria, piso=True, fecha_nacimiento=date(2000,12,30),lugar="Sevilla")
         
-        Sara= Usuario.objects.create(usuario=userSara, piso=True,fecha_nacimiento=date(2000,12,29)
-        ,edad=20,lugar="Cádiz")
+        Sara= Usuario.objects.create(usuario=userSara, piso=True,fecha_nacimiento=date(2000,12,29),lugar="Cádiz")
     
     
 
@@ -174,7 +171,7 @@ class LoginTest(TestCase):
     def setUp(self):
         user = User(username='usuario')
         user.set_password('qwery')
-        perfil = Usuario(usuario=user,piso=True,fecha_nacimiento="2000-1-1",edad=1,lugar="Sevilla",nacionalidad="Española",
+        perfil = Usuario(usuario=user,piso=True,fecha_nacimiento="2000-1-1",lugar="Sevilla",nacionalidad="Española",
                 genero='F',pronombres="Ella",idiomas="ES",universidad="US",estudios="Informática")
         user.save()
         perfil.save()
@@ -215,12 +212,9 @@ class NotificacionesTest(TestCase):
         user3.save()
 
 
-        pepe= Usuario.objects.create(usuario=user, piso=False, fecha_nacimiento=date(2000,12,31),
-        edad=20,lugar="Sevilla")
-        maria=Usuario.objects.create(usuario=user2, piso=True, fecha_nacimiento=date(2000,12,30),
-        edad=20,lugar="Sevilla")
-        sara= Usuario.objects.create(usuario=user3, piso=True,fecha_nacimiento=date(2000,12,29)
-        ,edad=20,lugar="Cádiz")
+        pepe= Usuario.objects.create(usuario=user, piso=False, fecha_nacimiento=date(2000,12,31),lugar="Sevilla")
+        maria=Usuario.objects.create(usuario=user2, piso=True, fecha_nacimiento=date(2000,12,30),lugar="Sevilla")
+        sara= Usuario.objects.create(usuario=user3, piso=True,fecha_nacimiento=date(2000,12,29),lugar="Cádiz")
 
         #MATE ENTRE user y user2
         mate1 = Mates.objects.create(mate=True,userEntrada=user, userSalida=user2)
