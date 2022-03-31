9e6f1aa5f58936f13bbd6e5a0d6497efb8181595
fergongar1 <fergongar1@alum.us.es>
2022-03-31 18:08:45 +0200

    B-016: MEJORA DEL SISTEMA DE RECOMENDACIÓN
    Se ha replanteado el sistema de recomendación para que pondere de manera
    más correcta las preferencias respecto a las tags.

--- MakeAMate/principal/recommendations.py
+++ MakeAMate/principal/recommendations.py
@@ -1,25 +1,32 @@
-PONDERACION_TAGS = 0.5
-PONDERACION_AFICIONES = 0.2
-PONDERACION_PREMIUM = 0.3
+# La ponderación de los gustos en la puntuación respecto a las tags.
+# Menor que uno: Los gustos se consideran menos importantes que las tags. Mayor que uno: Se consideran más.
+PONDERACION_AFICIONES = 0.5
+
+# El bonus por el que se multiplica la puntuación de un usuario si es premium.
+# Debe ser mayor que uno.
+BONUS_PREMIUM = 1.2
 
 def rs_score(us_entrada,us_salida):
-    tags_entrada = us_entrada.tags.all()
-    tags_salida = us_salida.tags.all()
-    similitud_tags = dice_coefficient(set(tags_entrada),set(tags_salida))
+    tags_entrada = set(us_entrada.tags.all())
+    tags_salida = set(us_salida.tags.all())
 
-    gustos_entrada = us_entrada.aficiones.all()
-    gustos_salida = us_salida.aficiones.all()
-    similitud_gustos = dice_coefficient(set(gustos_entrada),set(gustos_salida))
+    aficiones_entrada = set(us_entrada.aficiones.all())
+    aficiones_salida = set(us_salida.aficiones.all())
 
-    es_premium = 0 if us_salida.es_premium else 1
+    es_premium = us_salida.es_premium()
+    similitud = dice_coefficient(tags_entrada,tags_salida,aficiones_entrada,aficiones_salida)
 
-    score =  similitud_tags*PONDERACION_TAGS + similitud_gustos*PONDERACION_AFICIONES + es_premium*PONDERACION_PREMIUM
-
-    score = score if score<=1.0 else 1.0
+    score =  similitud*BONUS_PREMIUM if es_premium else similitud
     #print(us_entrada.usuario.username + " - " + us_salida.usuario.username +": " + str(score) +","+str(es_premium))
 
     return score
 
 
-def dice_coefficient(set1, set2):
-    return 2 * len(set1.intersection(set2)) / (len(set1) + len(set2))
+def dice_coefficient(tag1, tag2, pref1, pref2):
+    aficiones_comunes = len(pref1.intersection(pref2))
+    aficiones_total = len(pref1) + len(pref2)
+
+    tags_comunes  = len(tag1.intersection(tag2))
+    tags_total = len(tag1) + len(tag2)
+
+    return 2 * (tags_comunes + PONDERACION_AFICIONES*aficiones_comunes) / (tags_total + PONDERACION_AFICIONES*aficiones_total)
