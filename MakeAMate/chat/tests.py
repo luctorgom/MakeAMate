7d6aa26a147d34dbc54a2b1f4b8b0726e255efa5
alvesccas221 <alvesccas@alum.us.es>
2022-04-03 19:45:52 +0200

    TEST DE CONSUMERS

--- MakeAMate/chat/tests.py
+++ MakeAMate/chat/tests.py
@@ -1,9 +1,14 @@
+
 from tabnanny import verbose
 from django.test import TestCase, Client
-from django.contrib import auth
 from .models import ChatRoom
 from principal.models import Usuario, Mates
 from django.contrib.auth.models import User
+from channels.testing import WebsocketCommunicator
+from chat.consumers import WebsocketConsumer
+from channels.testing import WebsocketCommunicator
+
+import pytest
 
 # Create your tests here.
 
@@ -104,6 +109,11 @@ class ChatTest(TestCase):
         self.assertEqual(len(response.context['nombrechats']),5)
 
 
-
-
-
+    @pytest.mark.asyncio
+    async def test_consumer(self):
+        application = WebsocketConsumer.as_asgi()
+        communicator = WebsocketCommunicator(application, path="/chat/1/")
+        connected, subprotocol = await communicator.connect()
+        assert connected
+        await communicator.send_to(text_data="hola")
+        await communicator.disconnect()
