8e8457673c65e7f5ac7f0056d6b340cf7909c6df
alvesccas221 <80113459+alvesccas221@users.noreply.github.com>
2022-04-06 23:45:31 +0200

    Update settings.py

--- MakeAMate/MakeAMate/settings.py
+++ MakeAMate/MakeAMate/settings.py
@@ -91,7 +91,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONGIF": {
-            "hosts": [("https://makeamate2022-s2.herokuapp.com", 6379), ("127.0.0.1", 6379)]
+            "hosts": [("https://makeamate2022-s2.herokuapp.com", 6379)]
         }
     }
 }
