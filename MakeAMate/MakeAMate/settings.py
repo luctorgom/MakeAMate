c9132f693a3959795606e0479b532bfa025cafa4
alvesccas221 <alvesccas@alum.us.es>
2022-04-19 17:44:46 +0200

    PRUEBA CHAT DESPLIEGUE

--- MakeAMate/MakeAMate/settings.py
+++ MakeAMate/MakeAMate/settings.py
@@ -86,19 +86,19 @@ DATABASES = {
     }
 }
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels.layers.InMemoryChannelLayer"
-#     }
-# }
-
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis-server-name", 6379)],
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
+
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("redis-server-name", 6379)],
+#         },
+#     },
 }
 
 # Password validation
