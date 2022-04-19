522d0c588579b2f780a64387ec4c733c0baf1a6e
alvesccas221 <alvesccas@alum.us.es>
2022-04-20 00:59:47 +0200

    MUCHOS ARREGLOS
    Se arregla el room.html y se cambian algunas cosas en el views para pasar más parámetros

--- MakeAMate/chat/views.py
+++ MakeAMate/chat/views.py
@@ -69,12 +69,16 @@ def room(request, room_name):
         else:
             nombre_sala = chatroom.participants.all().filter(~Q(id=request.user.id))[0].username
 
-
+        usuario_opuesto = ""
+        if es_grupo== False:
+            usuario_opuesto = Usuario.objects.filter(id=chatroom.participants.all().filter(~Q(id=request.user.id))[0].id)[0]
         # last_message_decoded = Fernet(chatroom.publicKey.encode()).decrypt(bytes(Chat.objects.filter(timestamp = chatroom.last_message)[0].content,'utf-8')).decode()
 
         # Comprobación si el usuario pertenece a los participantes de ese grupo
         if request.user.username in lista_participantes :
-            return render(request, 'chat/room.html', {'room_name': room_name,'users': lista_mates, 'chats':lista_chat, 'nombrechats':lista_usuarios, 'form':form, 'nombre_sala':nombre_sala, 'es_grupo':es_grupo, 'last_message':lista_last_message})
+            return render(request, 'chat/room.html', {'room_name': room_name,'users': lista_mates, 'chats':lista_chat, 'nombrechats':lista_usuarios, 
+                                                    'form':form, 'nombre_sala':nombre_sala, 'es_grupo':es_grupo, 'last_message':lista_last_message,
+                                                    'usuario_actual':Usuario.objects.filter(id=request.user.id)[0], 'usuario_opuesto':usuario_opuesto})
         else:
             raise PermissionDenied
     else:
