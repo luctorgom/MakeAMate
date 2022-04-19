2241e5cfe433fccff217b79b25850f365f104659
alvesccas221 <alvesccas@alum.us.es>
2022-04-19 17:35:27 +0200

    PRUEBA CHAT DESPLIEGUE

--- MakeAMate/MakeAMate/settings.py
+++ MakeAMate/MakeAMate/settings.py
@@ -94,9 +94,9 @@ DATABASES = {
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [("redis-server-name", 6379)],
         },
     },
 }
