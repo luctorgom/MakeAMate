91b504042e2ee61f1089f24732bb390993544d61
alvesccas221 <alvesccas@alum.us.es>
2022-04-19 17:09:58 +0200

    PRUEBA CHAT DESPLIEGUE

--- MakeAMate/MakeAMate/asgi.py
+++ MakeAMate/MakeAMate/asgi.py
@@ -10,13 +10,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 from django import setup
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MakeAMate.settings")
+setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MakeAMate.settings")
-setup()
+
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
