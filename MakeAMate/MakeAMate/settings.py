8581a8e07aa8d07cb3612ec290bd31f31c127797
alvesccas221 <alvesccas@alum.us.es>
2022-04-24 12:15:46 +0200

    ARREGLO DESPLIEGUE

--- MakeAMate/MakeAMate/settings.py
+++ MakeAMate/MakeAMate/settings.py
@@ -145,8 +145,8 @@ STATICFILES_FINDERS = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 NOSE_ARGS = ['--with-xunit']
-#import django_heroku
-#django_heroku.settings(locals(),test_runner=False)
+import django_heroku
+django_heroku.settings(locals(),test_runner=False)
 
 DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
