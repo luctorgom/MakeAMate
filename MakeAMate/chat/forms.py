4a39120daa861de5a2c07bd54f3b7bb99337f1b5
Adriana <adrianalg2000@hotmail.com>
2022-04-24 19:04:28 +0200

    MÁS ARREGLOS CHAT
    Faltaban algunas cosas.
    
    León Gómez, Adriana <@adrleogom>

--- MakeAMate/chat/forms.py
+++ MakeAMate/chat/forms.py
@@ -9,15 +9,20 @@ class CrearGrupo(forms.Form):
             widget = forms.CheckboxSelectMultiple,
             choices=tuple([(name.id, name) for name in user_list]),
             )
-        self.fields['Nombre'].error_messages = {'required': 'Seleccione un nombre para el grupo'}
+        self.fields['Nombre'].error_messages = {'required': 'Seleccione un nombre para el grupo no mayor a 30 caracteres'}
          
-        self.fields['Personas'].error_messages = {'required': 'Seleccione al menos dos usuarios'}
+        self.fields['Personas'].error_messages = {'required': 'Seleccione al menos dos usuarios y no más de 10'}
     Nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'nombrePartitipante'}))
     Personas = forms.MultipleChoiceField()
 
-
     def clean_Personas(self):
         gente = self.cleaned_data.get('Personas')
-        if len(gente) < 2:
+        if len(gente) < 2 or len(gente)>10:
             raise forms.ValidationError('Prueba')
         return gente
+
+    def clean_Nombre(self):
+        nombre = self.cleaned_data.get('Nombre')
+        if len(nombre) > 40:
+            raise forms.ValidationError('Prueba')
+        return nombre
