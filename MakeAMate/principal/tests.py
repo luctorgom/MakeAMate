aac458d39e16e75fc9dd5a090f2b63399672a5e0
alvesccas221 <alvesccas@alum.us.es>
2022-04-26 19:51:53 +0200

    PRUEBA

--- MakeAMate/principal/tests.py
+++ MakeAMate/principal/tests.py
@@ -950,34 +950,39 @@ class DetallesPerfil(TestCase):
         userMaria=User(username="Maria")
         userMaria.set_password("asdfg")
         userMaria.save()
-
         piso_maria = Piso.objects.create(zona="Calle Marqués Luca de Tena 3", descripcion="Descripción de prueba 2")
-    
-        maria=Usuario.objects.create(usuario=userMaria, fecha_nacimiento=date(2000,12,30),lugar="Sevilla", piso=piso_maria, telefono="+34666777222", sms_validado=True)
+        maria=Usuario.objects.create(usuario=userMaria, fecha_nacimiento="2000-1-1",lugar="Sevilla", telefono="+34666777222",genero='F',estudios="Informática", sms_validado=True)
         maria.save()
 
         userPepe = User(username='usuario2')
         userPepe.set_password('qwery')
         userPepe.save()
-
-        pepe= Usuario.objects.create(id=11, usuario=userPepe, fecha_nacimiento=date(2000,12,31),lugar="Sevilla", telefono='+34111222333', sms_validado=True)
+        pepe= Usuario.objects.create(usuario=userPepe, fecha_nacimiento="2000-1-1",lugar="Sevilla", telefono='+34111222333',genero='F',estudios="Informática", sms_validado=True)
         pepe.save()
 
         user_no_sms = User(username='noSMS')
         user_no_sms.set_password('qwery')
         user_no_sms.save()
-
-        noSMS = Usuario.objects.create(usuario=user_no_sms, fecha_nacimiento=date(2000,12,31), lugar="Sevilla", telefono="+34666555444", sms_validado=False)
+        noSMS = Usuario.objects.create(usuario=user_no_sms, fecha_nacimiento="2000-1-1", lugar="Sevilla", telefono="+34666555444",genero='F',estudios="Informática", sms_validado=False)
         noSMS.save()
 
         #Pepe le da like a Maria
-        mate12 = Mate.objects.create(mate=True,userEntrada=userMaria, userSalida=userPepe)
+        mate12 = Mate.objects.create(mate=True,userEntrada=userPepe, userSalida=userMaria)
         mate12.save()
+
+        
+        
+        
+
+
+        
+        
+        
 
     #María entra en Make A Mate y ve el perfil de Pepe
     def test_positive_detalles(self):
         c = Client()
-        c.post('/login/', {'username': 'Maria', 'pass': 'asdfg'})
+        login = c.login(username="Maria", password="asdfg")
         id_user_pepe = str(Usuario.objects.get(telefono="+34111222333").id)
         print("///////////////////////////////////////////////////////////////////////////////////")
         print(id_user_pepe)
@@ -986,7 +991,7 @@ class DetallesPerfil(TestCase):
         url = "/details-profile/" + id_user_pepe
         response = c.get(url)
 
-        self.assertTrue(response.status_code == 200)
+        self.assertEqual(response.status_code, 200)
 
     #Pepe entra en Make A Mate y no puede ver el perfil de María
     def test_negative_detalles_no_mate(self):
@@ -997,7 +1002,7 @@ class DetallesPerfil(TestCase):
         response = c.get(url)
 
         #Como maría no le ha dado like a pepe, entonces este no puede acceder a su perfil y es redirigido a homepage
-        self.assertTrue(response.status_code == 302)
+        self.assertEqual(response.status_code, 302)
 
     def test_negative_detalles_no_login(self):
         c = Client()
